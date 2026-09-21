9cc2667b12d69bd38decf040f2302f46c66d3feb
Shashank HN <shashankhn405@gmail.com>
2026-09-21 22:45:28 +0530

    Update CourseContext.jsx

--- course-management-frontend/src/context/CourseContext.jsx
+++ course-management-frontend/src/context/CourseContext.jsx
@@ -1,44 +1,74 @@
-import axios, { all } from "axios";
+import axios from "axios";
 import React, { createContext, useEffect, useState } from "react";
 
 export let CourseProvider = createContext();
+
 const CourseContext = ({ children }) => {
-  let [allCourses, setAllCourses] = useState(null); // [{} , {} , {}]
-  let findById = (id)=>{
-    let data = allCourses?.find((el)=>{
-        return el.id == id
-    })
+  let [allCourses, setAllCourses] = useState(null);
+
+  const API_URL = import.meta.env.VITE_API_URL;
+
+  let findById = (id) => {
+    let data = allCourses?.find((el) => {
+      return el.id == id;
+    });
+
     return data;
-  }
+  };
+
   let getAllCourses = async () => {
-    let res = await axios.get("http://localhost:5000/courses");
-    if(res.status == 200 && res.data.length>0){
-        setAllCourses(res.data)
+    try {
+      let res = await axios.get(`${API_URL}/courses`);
+
+      if (res.status == 200 && res.data.length > 0) {
+        setAllCourses(res.data);
+      }
+    } catch (error) {
+      console.log("Error fetching courses:", error);
     }
   };
 
-  let handleAddCourse = (courseData)=>{
-    setAllCourses([...allCourses ,courseData ])
-  }
-  let handleUpdateCourse = (courseData)=>{
-    let updated = allCourses.map((course)=>{
-      return course.id == courseData.id ? courseData :course;
-    })
-    setAllCourses(updated)
-  }
+  let handleAddCourse = (courseData) => {
+    setAllCourses([...allCourses, courseData]);
+  };
 
-  let deleteById = async(id)=>{
-    let filtered = allCourses?.filter((el)=>el.id != id)
-    setAllCourses(filtered)
-    let res = await axios.delete(`http://localhost:5000/courses/${id}`)
-    console.log(res);
-  }
+  let handleUpdateCourse = (courseData) => {
+    let updated = allCourses.map((course) => {
+      return course.id == courseData.id ? courseData : course;
+    });
+
+    setAllCourses(updated);
+  };
+
+  let deleteById = async (id) => {
+    try {
+      let filtered = allCourses?.filter((el) => el.id != id);
+      setAllCourses(filtered);
+
+      let res = await axios.delete(`${API_URL}/courses/${id}`);
+
+      console.log(res);
+    } catch (error) {
+      console.log("Error deleting course:", error);
+    }
+  };
+
   useEffect(() => {
     getAllCourses();
   }, []);
 
   return (
-    <CourseProvider.Provider value={{allCourses,handleUpdateCourse , handleAddCourse , findById , deleteById}}>{children}</CourseProvider.Provider>
+    <CourseProvider.Provider
+      value={{
+        allCourses,
+        handleUpdateCourse,
+        handleAddCourse,
+        findById,
+        deleteById,
+      }}
+    >
+      {children}
+    </CourseProvider.Provider>
   );
 };
 
